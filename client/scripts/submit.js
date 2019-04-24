883f196ee99d374acae277ba4a424f1b93273604
dopeness <infinitedopeness@gmail.com>
2019-04-24 12:37:47 -0700

    changes for summit

--- client/scripts/submit.js
+++ client/scripts/submit.js
@@ -4,20 +4,25 @@ console.log("File Detected");
 $(document).ready(function() {
 console.log( "jquery is loaded after document ready from index.html");
 
-    $("#request-button").click(()=> {
-      let dataToSendToServer = {"data": "hello server"};
+    $("#save").click(()=> {
+      console.log("something");
+
+      let value = $("#number").val();
+
+
+      let dataToSendToServer = {"userNumber": value};
         //$.post("location","data",callback function)
         //$.post("http://localhost:3000,"retrieveList","hello server",function(){
         //  console.log("the server  has responded")   // below using es6 notation
         //making a post request
-          $.post("http://localhost:3000",dataToSendToServer, (response) => {
-            console.log("the server  has responded");
-            console.log("the server sent the following:", response);
+          $.post("http://localhost:3000/numberSaver",dataToSendToServer, (data) => {
+            console.log("the number was sent to the server");
+            //console.log("the server sent the following:", data);
 
           });
 
     });
-    
+
 
 });  // jquery sending a string to the Server
 
