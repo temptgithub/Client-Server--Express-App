3039d5ee434718a6355a6f1498e0c5ec408fba42
dopeness <infinitedopeness@gmail.com>
2019-04-24 11:14:46 -0700

    changes to CS program

--- client/scripts/scripts.js
+++ client/scripts/scripts.js
@@ -1,12 +1,22 @@
 "use strict";
 console.log("File Detected");
-
+// below are jquery
 $(document).ready(function() {
 console.log( "jquery is loaded after document ready from index.html");
-});
 
-$.post("http://localhost:3000,retrieveList","sent string hello server",function(){
-  console.log("the server  has responded")
-});
+    $("#request-button").click(()=> {
+        //$.post("location","data",callback function)
+        //$.post("http://localhost:3000,"retrieveList","hello server",function(){
+        //  console.log("the server  has responded")   // below using es6 notation
+        //making a post request
+          $.post("http://localhost:3000",{"data": "hello server"}, () => {
+            console.log("the server  has responded");
+
+          });
+
+    });
+
+});  // jquery sending a string to the Server
+
 
 console.log("script finished");
--- index.js
+++ index.js
@@ -1,20 +1,30 @@
 const express = require("express");    // this is all you need for a server
-
 const app = express();    // variable run as a function
+const bodyParser = require("body-parser");
 
 let http = require("http").Server(app);  //class with a Server Method that accepts express server as variable intake
 
 app.use("/", express.static("./client") ); // this is in reference to the html?
-
 // app.use("/secretfolder", express.static("private/") );   // setting up different static directory roots -  routing to different folder
+// following two lines are for body parser to understand objects thru post requests
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
 
-let port =3000;
+
+const port =3000;
 
 http.listen(port);                 // for http requests or any port that is open
 
 console.log(`Express server is now running on port  ${port} `);
+//handling a post request  eg run below made from post button
+app.post("/",(request,response)=>{
+  let dataRecieved = request.body;
 
-
+  console.log("someone made a request");
+  console.log(`the request sent the following to us ` ,dataRecieved );
+  // cannot do string concatenation
+    response.send(" hi i received your message");
+});
 /*
 let myVariable = function() {
   console.log("hello world")
